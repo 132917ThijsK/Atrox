5a5f569357bb46f490a0a4d9a673be87d26010b6
132917 <132917@hondsrugcollege.nl>
2025-03-05 14:33:40 +0100

    Update

--- MCreatorWorkspaces/Atrox/src/main/java/net/mcreator/atrox/init/AtroxModEntities.java
+++ MCreatorWorkspaces/Atrox/src/main/java/net/mcreator/atrox/init/AtroxModEntities.java
@@ -74,9 +74,14 @@ public class AtroxModEntities {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 		event.put(BOOSSHIELD.get(), BoosshieldEntity.createAttributes().build());
 		event.put(BOOSARTEMIS.get(), BoosartemisEntity.createAttributes().build());
 		event.put(CRACKED_ANGEL.get(), CrackedAngelEntity.createAttributes().build());
+=======
+		event.put(BOSSSHIELD.get(), BoosshieldEntity.createAttributes().build());
+		event.put(BOSSARTEMIS.get(), BoosartemisEntity.createAttributes().build());
+>>>>>>> Stashed changes
 =======
 		event.put(BOSSSHIELD.get(), BoosshieldEntity.createAttributes().build());
 		event.put(BOSSARTEMIS.get(), BoosartemisEntity.createAttributes().build());
